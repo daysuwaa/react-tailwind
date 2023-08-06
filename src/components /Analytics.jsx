79b4bd455b5ff89185320e98e2748a144770e458
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-06 22:27:26 +0100

    add

--- src/components /Analytics.jsx
+++ src/components /Analytics.jsx
@@ -5,10 +5,10 @@ const Analytics = () => {
   return (
     <div className='w-full bg-white py-16 px-4'>
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
-            <img src={Laptop} alt ="/" />
-            <div>
-                <p>DATA ANALYTICS DASHBOARD</p>
-                <h1>Manage Data Analytics Centrally</h1>
+            <img className='w-[500px] mx-auto my-4' src={Laptop} alt ="/" />
+            <div className='flex flex-col justify-center'>
+                <p className='text-[#00df9a] font-bold'>DATA ANALYTICS DASHBOARD</p>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum porro omnis, 
                     adipisci rerum iure mollitia ut architecto, 
                     optio quas excepturi magnam accusamus repellendus voluptates itaque.
