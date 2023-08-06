e681727df64837d0bb841c793bd349b65ad9c3fb
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-06 17:31:39 +0100

    commit : made changes to depndencies

--- src/components /Hero.jsx
+++ src/components /Hero.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Typed from 'react-typed';
 
 const Hero = () => {
@@ -9,7 +9,11 @@ const Hero = () => {
           <h1 className='md:text-7xl sm:text:6xl text-4xl font-bold md:py-6'>Grow with data.</h1> 
           <div>
             <p className='md:text-5xl sm:text-4xl text-xl font-bold'>Fast, flexible financing for</p>
-            <Typed strings={['BTB', 'BTC' ,'SASS'] } typeSpeed={120} backSpeed={140} loop />
+            <Typed strings={['BTB', 'BTC' ,'SASS'] } 
+            typeSpeed={120} 
+            backSpeed={140} 
+            loop
+             />
           </div>
        </div>
     </div>
