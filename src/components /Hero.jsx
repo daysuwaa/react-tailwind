e42106ab98bf9a156e32024935339d8797c2984a
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-06 18:54:39 +0100

    Commit:Hero done

--- src/components /Hero.jsx
+++ src/components /Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
           <p className='text-[#00df9a] font-bold p-2'>
               GROWING WITH DATA ANALYTICS
           </p>
-          <h1 className='md:text-7xl sm:text:6xl text-4xl font-bold md:py-6'>
+          <h1 className='md:text-7xl sm:text:6xl text-4xl font-bold md:py-4'>
               Grow with data.
             </h1> 
           <div className='flex justify-center items-center'>
@@ -16,7 +16,7 @@ const Hero = () => {
                 Fast, flexible financing for
             </p>
             <Typed
-            className='md:text-5xl sm:text-4xl text-xl font-bold pl-2'
+            className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
             strings={['BTB', 'BTC' ,'SASS'] } 
             typeSpeed={120} 
             backSpeed={140} 
@@ -24,6 +24,7 @@ const Hero = () => {
              />
           </div>
           <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC & SASS platforms</p>
+          <button className='bg-[#00df9a] w-[200px] rounded-md my-6 mx-auto py-3 text-black'>Get Started</button>
        </div>
     </div>
   );
