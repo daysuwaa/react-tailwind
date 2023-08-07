bb5c5b157cc73475d1c94859298f026fecf06343
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-07 15:37:17 +0100

    Commit : FOOTER DONE.

--- src/components /Footer.jsx
+++ src/components /Footer.jsx
@@ -1,38 +1,37 @@
-import React from 'react'
+import React from 'react';
 import {
-    FaDribbbleSquare,
-    FaFacebookSquare,
-    FaGithubSquare,
-    FaInstagram,
-    FaTwitterSquare,
-  } from 'react-icons/fa';
+  FaDribbbleSquare,
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagram,
+  FaTwitterSquare,
+} from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div className='max-w[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
-        <div>
+    <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
+      <div>
         <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
-        <p className='py-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis tenetur facilis maiores consequuntur consequatur beatae dolore sint ducimus pariatur unde et eos, in iste officiis excepturi assumenda. Rerum, consectetur ducimus!</p>
-        <div className='flex md:w-[75%] my-6 justify-between'>
-            <FaFacebookSquare  size={30} />
+        <p className='py-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id odit ullam iste repellat consequatur libero reiciendis, blanditiis accusantium.</p>
+        <div className='flex justify-between md:w-[75%] my-6'>
+            <FaFacebookSquare size={30} />
             <FaInstagram size={30} />
             <FaTwitterSquare size={30} />
             <FaGithubSquare size={30} />
             <FaDribbbleSquare size={30} />
         </div>
-        </div>
-        <div className='lg:col-span-3 flex justify-between'>
-            <div>
-            <h6 className='font-medium text-gray-400'>Solutions</h6>
+      </div>
+      <div className='lg:col-span-2 flex justify-between mt-6'>
+    <div>
+        <h6 className='font-medium text-gray-400'>Solutions</h6>
         <ul>
             <li className='py-2 text-sm'>Analytics</li>
             <li className='py-2 text-sm'>Marketing</li>
             <li className='py-2 text-sm'>Commerce</li>
             <li className='py-2 text-sm'>Insights</li>
         </ul>
-     </div>
-     <div>
-     <div>
+    </div>
+    <div>
         <h6 className='font-medium text-gray-400'>Support</h6>
         <ul>
             <li className='py-2 text-sm'>Pricing</li>
@@ -41,7 +40,8 @@ const Footer = () => {
             <li className='py-2 text-sm'>API Status</li>
         </ul>
     </div>
-     <h6 className='font-medium text-gray-400'>Company</h6>
+    <div>
+        <h6 className='font-medium text-gray-400'>Company</h6>
         <ul>
             <li className='py-2 text-sm'>About</li>
             <li className='py-2 text-sm'>Blog</li>
@@ -50,7 +50,6 @@ const Footer = () => {
             <li className='py-2 text-sm'>Careers</li>
         </ul>
     </div>
-
     <div>
         <h6 className='font-medium text-gray-400'>Legal</h6>
         <ul>
@@ -59,10 +58,9 @@ const Footer = () => {
             <li className='py-2 text-sm'>Terms</li>
         </ul>
     </div>
- </div>
-   
-   </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
