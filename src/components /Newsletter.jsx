341e3b516ba1e65f40839473c3a5f76285019236
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-08-07 14:01:16 +0100

    newsletter

--- src/components /Newsletter.jsx
+++ src/components /Newsletter.jsx
@@ -4,18 +4,20 @@ const Newsletter = () => {
   return (
     <div className='w-full py-16 text-white'>
         <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
-            <div className='lg:col-span-2'>
+            <div className='lg:col-span-2 my-4'>
                 <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Want tips & tricks to optimize</h1>
                 <p>Sign up to our newsletter and stay up to date.</p>
             </div>
             <div className='my-4'>
-                <div>
+                <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
                     <input 
+                    className='p-3 flex w-full rounded-md text-black'
                     type='email' 
                     placeholder='Enter Email'
                      />
                      <button className='bg-[#00df9a] w-[200px] rounded-md ml-4 px-6 my-6 mx-auto py-3 text-black'>Notify Me</button>
                 </div>
+                <p>We care bout the protection of your Data. Read our <span className='bg-[#00df9a]'>Privacy Policy.</span></p>
 
             </div>
         </div>
